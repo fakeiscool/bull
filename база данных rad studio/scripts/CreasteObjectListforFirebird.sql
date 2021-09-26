4bd40026607cb875ea39f1c71ef7daa220adbf51
fakeiscool <52255174+fakeiscool@users.noreply.github.com>
2021-09-26 13:18:46 +0300

    Delete база данных rad studio directory

--- база данных rad studio/scripts/CreasteObjectListforFirebird.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ObjectList
-(
-MID_OBJECT BIGINT NOT NULL,     //   код метаобъекта  --   метакод
-ID_OBJECT BIGINT NOT NULL,      //   код объекта
-CODE_TYPE BIGINT NOT NULL,      //   код типа объекта
-NAME_OBJECT VARCHAR(255) NOT NULL,    //    имя объекта
-CONSTRAINT PK_ListTypeObjectS PRIMARY KEY (MID_OBJECT)
-)
